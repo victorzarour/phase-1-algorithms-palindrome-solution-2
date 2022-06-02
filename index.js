a7ab8f8d5cc5c925fc0ba106cd3aeaccd3336d10
Victor <vx_zz@hotmail.com>
2022-06-02 15:50:10 -0400

    commit tree 8141628184d4a5413af40281ca3aec059303cede

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++){
+    const j = word.length -1 -i;
+    if (word[i] !== word[j]) return false
+  }
+  return true
 }
 
 /* 
